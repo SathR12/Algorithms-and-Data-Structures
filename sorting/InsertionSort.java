0df9dca9f613d8b291744c1a0e4c2a1c14b4da5f
SathR12 <74515743+SathR12@users.noreply.github.com>
2022-03-28 18:49:06 -0400

    Update InsertionSort.java

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -18,17 +18,4 @@ public class InsertionSort {
 
     }
 
-    static String output(int[] arr) {
-        String str = "[";
-        for (int i = 0; i < arr.length; i++) {
-            if (i == arr.length - 1) {
-                str += arr[i] + "]";
-            } else {
-                str += arr[i] + ", ";
-            }
-        }
-
-        return str;
-    }
-
 }
