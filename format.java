6d8d448f752fdcaa1dc9dde725a04863737e492f
SathR12 <74515743+SathR12@users.noreply.github.com>
2022-03-28 18:55:24 -0400

    Update format.java

--- format.java
+++ format.java
@@ -1,12 +1,14 @@
- static String output(int[] arr) {
-        String str = "[";
-        for (int i = 0; i < arr.length; i++) {
-            if (i == arr.length - 1) {
-                str += arr[i] + "]";
-            } else {
-                str += arr[i] + ", ";
-            }
-        }
+class ListOutput {
+    static String output(int[] arr) {
+         String str = "[";
+         for (int i = 0; i < arr.length; i++) {
+             if (i == arr.length - 1) {
+                 str += arr[i] + "]";
+             } else {
+                 str += arr[i] + ", ";
+             }
+         }
 
-        return str;
-    }
+         return str;
+     }
+}
