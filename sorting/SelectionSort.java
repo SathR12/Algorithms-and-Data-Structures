32ea84749d9af9e03cd5ff746666ea06a1abccc9
SathR12 <74515743+SathR12@users.noreply.github.com>
2022-03-28 18:49:40 -0400

    Update SelectionSort.java

--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -19,17 +19,5 @@ public class SelectionSort {
         return arr;
     }
 
-    static String output(int[] arr) {
-        String str = "[";
-        for (int i = 0; i < arr.length; i++) {
-            if (i == arr.length - 1) {
-                str += arr[i] + "]";
-            } else {
-                str += arr[i] + ", ";
-            }
-        }
-
-        return str;
-    }
 
 }
