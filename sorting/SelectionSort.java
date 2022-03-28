32040be63a48ad405f20f21ed57fe38d672d674f
SathR12 <74515743+SathR12@users.noreply.github.com>
2022-03-28 17:07:48 -0400

    Create SelectionSort.java

---
+++ sorting/SelectionSort.java
@@ -0,0 +1,35 @@
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] sort_arr = {4, 1, 25, 0, 123, 0, 4};
+        System.out.println("Selection sort: " + output(selectionSort(sort_arr)));
+    }
+
+    static int[] selectionSort(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+
+        return arr;
+    }
+
+    static String output(int[] arr) {
+        String str = "[";
+        for (int i = 0; i < arr.length; i++) {
+            if (i == arr.length - 1) {
+                str += arr[i] + "]";
+            } else {
+                str += arr[i] + ", ";
+            }
+        }
+
+        return str;
+    }
+
+}
