c0e0727a7161becdbd544a6b791444ea72dc5e48
SathR12 <74515743+SathR12@users.noreply.github.com>
2022-03-28 17:07:30 -0400

    Create InsertionSort.java

---
+++ sorting/InsertionSort.java
@@ -0,0 +1,34 @@
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] sort_arr = {4, 1, 25, 0, 123, 0, 4};
+        System.out.println("Insertion sort: " + output(insertionSort(sort_arr)));
+    }
+
+    static int[] insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int prev = i - 1;
+            while (prev >= 0 && arr[prev] > key) {
+                arr[prev + 1] = arr[prev];
+                prev -= 1;
+            }
+            arr[prev + 1] = key;
+        }
+        return arr;
+
+    }
+
+    static String output(int[] arr) {
+        String str = "[";
+        for (int i = 0; i < arr.length; i++) {
+            if (i == arr.length - 1) {
+                str += arr[i] + "]";
+            } else {
+                str += arr[i] + ", ";
+            }
+        }
+
+        return str;
+    }
+
+}
